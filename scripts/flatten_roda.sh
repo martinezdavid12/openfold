57814a86c1afeef38dd0b0c6a05dc1117ddea90c
David <martinezdavid@berkeley.edu>
2025-04-20 14:47:19 -0700

    metadata and aux files for training

--- scripts/flatten_roda.sh
+++ scripts/flatten_roda.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 #
 # Flattens a downloaded RODA database into the format expected by OpenFold
 # Args:
@@ -9,7 +9,7 @@
 #     output_dir:
 #           The directory in which to construct the reformatted data
 
-if [[ $# != 2 ]]; then
+if [[ $# -ne 2 ]]; then
     echo "usage: ./flatten_roda.sh <roda_dir> <output_dir>"
     exit 1
 fi
@@ -26,22 +26,23 @@ mkdir -p "${ALIGNMENT_DIR}"
 for chain_dir in $(ls "${RODA_DIR}"); do
     CHAIN_DIR_PATH="${RODA_DIR}/${chain_dir}"
     for subdir in $(ls "${CHAIN_DIR_PATH}"); do
-        if [[ ! -d "$subdir" ]]; then
-            echo "$subdir is not directory"
+        FULL_SUBDIR_PATH="${CHAIN_DIR_PATH}/${subdir}"
+        if [[ ! -d "${FULL_SUBDIR_PATH}" ]]; then
+            echo "${FULL_SUBDIR_PATH} is not a directory"
             continue
-        elif [[ -z $(ls "${subdir}")]]; then
+        elif [[ -z "$(ls -A "${FULL_SUBDIR_PATH}")" ]]; then
             continue
-        elif [[ $subdir = "pdb" ]] || [[ $subdir = "cif" ]]; then
-            mv "${CHAIN_DIR_PATH}/${subdir}"/* "${DATA_DIR}"
+        elif [[ "${subdir}" == "pdb" ]] || [[ "${subdir}" == "cif" ]]; then
+            mv "${FULL_SUBDIR_PATH}"/* "${DATA_DIR}/"
         else
             CHAIN_ALIGNMENT_DIR="${ALIGNMENT_DIR}/${chain_dir}"
             mkdir -p "${CHAIN_ALIGNMENT_DIR}"
-            mv "${CHAIN_DIR_PATH}/${subdir}"/* "${CHAIN_ALIGNMENT_DIR}"
+            mv "${FULL_SUBDIR_PATH}"/* "${CHAIN_ALIGNMENT_DIR}/"
         fi
     done
 done
 
 NO_DATA_FILES=$(find "${DATA_DIR}" -type f | wc -l)
-if [[ $NO_DATA_FILES = 0 ]]; then
-    rm -rf ${DATA_DIR}
+if [[ ${NO_DATA_FILES} -eq 0 ]]; then
+    rm -rf "${DATA_DIR}"
 fi
